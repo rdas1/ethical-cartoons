6f6f02f50649b47b603b359f1894d7270d978653
rdas1 <lilmessi42@gmail.com>
2025-04-27 00:41:10 -0400

    updated frontend base url

--- backend/app/utils/constants.py
+++ backend/app/utils/constants.py
@@ -1,2 +1,2 @@
 import os
-FRONTEND_BASE_URL = os.environ.get("FRONTEND_BASE_URL", "http://localhost:5173/ethical-cartoons")
+FRONTEND_BASE_URL = os.environ.get("FRONTEND_BASE_URL", "https://rdas1.github.io/ethical-cartoons")
